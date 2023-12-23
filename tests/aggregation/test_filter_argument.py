2e116a4621f3bb188941bb208db6fa9c297e2587
Lin Zhiwen <zhiwenlin1116@gmail.com>
2023-12-23 23:46:49 +0800

    test: fix test failures caused by enable DatabaseFeatures.supports_aggregate_filter_clause

--- tests/aggregation/test_filter_argument.py
+++ tests/aggregation/test_filter_argument.py
@@ -92,7 +92,7 @@ class FilteredAggregateTests(TestCase):
     def test_plain_annotate(self):
         agg = Sum("book__pages", filter=Q(book__rating__gt=3))
         qs = Author.objects.annotate(pages=agg).order_by("pk")
-        self.assertSequenceEqual([a.pages for a in qs], [447, None, 1047])
+        self.assertSequenceEqual([a.pages for a in qs], [447, 0, 1047])
 
     def test_filtered_aggregate_on_annotate(self):
         pages_annotate = Sum("book__pages", filter=Q(book__rating__gt=3))
