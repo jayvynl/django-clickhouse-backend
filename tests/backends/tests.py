8e31c32cf349f10acc1a35afaf0517f0f6258357
Pax <13646646+paxcodes@users.noreply.github.com>
2024-07-24 10:14:51 -0700

    chore: Resolve lint error

--- tests/backends/tests.py
+++ tests/backends/tests.py
@@ -132,7 +132,7 @@ class LastExecutedQueryTest(TestCase):
     @skipUnlessDBFeature("supports_paramstyle_pyformat")
     def test_last_executed_query_params_dict(self):
         square_opts = Square._meta
-        sql = "SELECT %s, %s FROM %s WHERE %s =" %  (
+        sql = "SELECT %s, %s FROM %s WHERE %s =" % (
             connection.ops.quote_name(square_opts.get_field("root").column),
             connection.ops.quote_name(square_opts.get_field("square").column),
             connection.introspection.identifier_converter(square_opts.db_table),
