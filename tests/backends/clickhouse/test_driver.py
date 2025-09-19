03b9adb4d27919c3e3e197d48348c89c0d4a7f62
Caitriona McCaffrey <158089298+caitriona-cloudsmith@users.noreply.github.com>
2025-09-19 04:05:33 +0100

    Fix for simultaneous queries issue when iterating  (#133)
    * Demonstrating breaking connection
    
    * Stop reusing broken connection
    
    * Added changelog and explanatory comment to test

--- tests/backends/clickhouse/test_driver.py
+++ tests/backends/clickhouse/test_driver.py
@@ -1,6 +1,9 @@
+from django.db import connection
 from django.test import TestCase
 
 from clickhouse_backend.driver import connect
+
+from .. import models
 
 
 class Tests(TestCase):
@@ -9,3 +12,41 @@ class Tests(TestCase):
         assert conn.pool.connections_min == 2
         assert conn.pool.connections_max == 4
         assert len(conn.pool._pool) == 2
+
+
+class IterationTests(TestCase):
+    """
+    Testing connection behaviour when iterating over queryset is interrupted.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.a1, cls.a2, cls.a3 = models.Author.objects.bulk_create(
+            [
+                models.Author(name="a1"),
+                models.Author(name="a2"),
+                models.Author(name="a3"),
+            ]
+        )
+
+    def test_connection_not_reused_when_iteration_interrupted(self):
+        """
+        This test demonstrates that if a queryset is iterated over and the
+        iteration is interrupted (e.g. via a break statement), the connection
+        used for that iteration is disconnected and not returned to the pool.
+        """
+        pool = connection.connection.pool
+
+        connection_count_before = len(pool._pool)
+        assert connection_count_before == 1
+
+        authors = models.Author.objects.all()
+        for author in authors.iterator(1):
+            author = author.name
+            break
+
+        connection_count_after_iterator = len(pool._pool)
+        # Connection was closed and not returned to pool
+        assert connection_count_after_iterator == 0
+
+        author = authors.get(id=self.a1.id)
